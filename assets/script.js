647224e1ec463670a3ec2d99813cbc9aa14341f0
Alexis <alexis_keeter@yahoo.com>
2023-03-28 15:47:54 -0700

    added document.ready to function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 var timeSectionEl = $('#currentDay');
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-(function () {
+$(document).ready(function () {
  
   var hour = dayjs().hour()
   console.log(hour);
